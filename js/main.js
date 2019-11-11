87de0e663ee0370157064354768f20a9389aa208
jscripter78 <50518314+jscripter78@users.noreply.github.com>
2019-11-11 12:19:10 +0300

    lesson3_3

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
   $(".owl-carousel").owlCarousel({
-    loop: true,
+    // loop: true,
     margin: 10,
     nav: true,
+    navClass: ["slider__nav--left", "slider__nav--right"],
     responsive: {
       0: {
         items: 1
       },
-      600: {
+      700: {
+        items: 2
+      },
+      900: {
         items: 3
       },
-      1000: {
-        items: 5
+      1200: {
+        items: 4
       }
     }
   });
